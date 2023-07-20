2ced8f83a294e8463e6fe58a1d011543ced0abd0
Felipe Martins <fe.mmo515@gmail.com>
2023-07-19 23:26:06 -0300

    Update system.ts

--- frontend/src/utils/system.ts
+++ frontend/src/utils/system.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dslist-evento-production.up.railway.app";
